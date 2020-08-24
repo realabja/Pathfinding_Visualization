2be23780d0beb691bff2b847e0cae3f8aa1db1c0
abbas jafari <realabja@gmail.com>
2020-08-24 21:55:22 +0200

    now is the time that I realize It should have been Writen using objects

--- main.js
+++ main.js
@@ -5,23 +5,94 @@ var squareHeight = display.offsetHeight/width;
 
 // var extraPixels = display.offsetHeight - (width*squareHeight);
 // console.log(extraPixels);
+
+function print(toPrint){
+    console.log(toPrint);
+}
 // display.style.cssText = 'margin-right: -' + extraPixels +'px;';
  
 
 
 var grid = [];
+var gameGrid = [];
 var x = 0;
 var y = 0;
-while(x<width){
+while(y<width){
     grid.push([]);
-    while(y<width){
-        display.insertAdjacentHTML('beforeend', `<div class="box" style="height: ${squareHeight}px; width: ${squareHeight}px;" id="${x}-${y}"></div>`);
-        grid[x].push(document.getElementById(""+x+"-"+y+""));
-        y++;
+    gameGrid.push([]);
+    while(x<width){
+        display.insertAdjacentHTML('beforeend', `<div class="box" style="height: ${squareHeight}px; width: ${squareHeight}px;" id="${y}-${x}"></div>`);
+        gameGrid[y].push(null);
+        grid[y].push(document.getElementById(""+y+"-"+x+""));
+        x++;
         }
-    x++;
-    y = 0;
+    y++;
+    x = 0;
 }
+
+function sleep(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
+
+function getCoordinates(box){
+    return box.id.split("-");
+}
+
+function block(box){
+    box.style.backgroundColor = "black";
+    coordinates = getCoordinates(box);
+    gameGrid[coordinates[0]][coordinates[1]] = "block";
+}
+
+function start(box){
+    box.style.backgroundColor = "green";
+    coordinates = getCoordinates(box);
+    gameGrid[coordinates[0]][coordinates[1]] = "start";
+    return coordinates;
+}
+
+function end(box){
+    box.style.backgroundColor = "blue";
+    coordinates = getCoordinates(box);
+    gameGrid[coordinates[0]][coordinates[1]] = "end";
+    return coordinates;
+}
+function getDistance(coordinates){
+    jDistance = Math.abs( coordinates[0] - endCoordinates[0] );
+    iDistance = Math.abs( coordinates[1] - endCoordinates[1] );
+    return Math.sqrt( Math.pow(jDistance,2) + Math.pow(iDistance,2) );
+}
+function walkto(box) {
+    box.style.backgroundColor = "red";
+}
+function neighbors(box) {
+    c = getCoordinates(box);
+    neighborsCo = []
+    neighborsCo.push([c[0],c[1]+1]);
+    neighborsCo.push([c[0]+1,c[1]+1]);
+    neighborsCo.push([c[0]+1,c[1]]);
+    neighborsCo.push([c[0]+1,c[1]-1]);
+    neighborsCo.push([c[0],c[1]-1]);
+    neighborsCo.push([c[0]-1,c[1]-1]);
+    neighborsCo.push([c[0]-1,c[1]]);
+    neighborsCo.push([c[0]-1,c[1]+1]);
+    for (i=0; i<neighborsCo.length; i++) {
+        if(neighborsCo[i][0]<0){
+            neighborsCo[i] = null
+        }
+        if(neighborsCo[i][1]<0){
+            neighborsCo[i] = null
+        }
+    }
+}
+
+
+
+var startCoordinates = start(grid[1][5]);
+var endCoordinates = end(grid[8][9]);
+block(grid[2][4]);
+print(gameGrid);
+print(getDistance(startCoordinates));
 
 
 
