f795b032152a7f71533b270a748cbd764cb99d0e
abbas jafari <realabja@gmail.com>
2020-08-24 11:23:53 +0200

    ...

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var display = document.querySelector("#visualization");
-var width = 30;
+var width = 50;
 var squareHeight = (display.offsetHeight-(2*width))/width;
 var extraPixels = display.offsetHeight - ((width*(squareHeight+2)) - 2);
 console.log(extraPixels);
@@ -12,7 +12,7 @@ var x = 0;
 var y = 0;
 while(x<width){
     while(y<width){
-        display.insertAdjacentHTML('beforeend', '<div class="box" style="height: ' + squareHeight + 'px; ' + 'width: ' + squareHeight + 'px;"' + 'id="' + y + '-' + x + '">' + '</div>');
+        display.insertAdjacentHTML('beforeend', `<div class="box" style="height: ${squareHeight}px; width: ${squareHeight}px;" id="${y}-${x}"></div>`);
         y++;
     }
     x++;
