cadec3dfdd3759423160a5de38585494a1164608
abbas jafari <realabja@gmail.com>
2020-08-24 16:00:58 +0200

    ...

--- main.js
+++ main.js
@@ -1,26 +1,35 @@
 var display = document.querySelector("#visualization");
-var width = 30;
+var width = 20;
 var squareHeight = display.offsetHeight/width;
-var extraPixels = display.offsetHeight - (width*squareHeight);
-console.log(extraPixels);
-display.style.cssText = 'margin-right: -' + extraPixels +'px;';
+
+
+// var extraPixels = display.offsetHeight - (width*squareHeight);
+// console.log(extraPixels);
+// display.style.cssText = 'margin-right: -' + extraPixels +'px;';
  
 
 
-
+var grid = [];
 var x = 0;
 var y = 0;
 while(x<width){
+    grid.push([]);
     while(y<width){
-        display.insertAdjacentHTML('beforeend', `<div class="box" style="height: ${squareHeight}px; width: ${squareHeight}px;" id="${y}-${x}"></div>`);
+        display.insertAdjacentHTML('beforeend', `<div class="box" style="height: ${squareHeight}px; width: ${squareHeight}px;" id="${x}-${y}"></div>`);
+        grid[x].push(document.getElementById(""+x+"-"+y+""));
         y++;
-    }
+        }
     x++;
     y = 0;
 }
 
 
 
-console.log(squareHeight)
+
+
+
+
+// console.log(grid)
+// console.log(squareHeight)
 // console.log(display.offsetHeight)
 // console.log(width)
